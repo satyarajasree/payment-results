814385a37388818f6e38da488795b4020a10bcea
Sathya7032 <sathichary581@gmail.com>
2025-04-09 16:36:52 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PaymentStatus from "./PaymentStatus";
+import Wish from "./Wish";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Wish/>} />
           <Route path="/payment-status" element={<PaymentStatus />} />
         </Routes>
       </BrowserRouter>
---
+++ src/Wish.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Wish = () => {
+  return (
+    <div>
+        <h1>Rajasree Townships web page </h1>
+    </div>
+  )
+}
+
+export default Wish
